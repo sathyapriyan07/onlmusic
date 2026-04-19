19fc1cfc305545bf48ef2e02713ea23752e854b6
Sathya Priyan <sathyapriyan710@gmail.com>
2026-04-19 14:22:12 +0530

    initial project

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'onl-music-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(cacheNames.map((name) => name !== CACHE_NAME && caches.delete(name)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
